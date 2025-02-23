c8b332cfab971788694091c6756be870182348f3
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-02-23 18:52:21 +0100

    Add track and trigger

--- src/main.js
+++ src/main.js
@@ -1,14 +1,42 @@
-let price = 5;
-let quantity = 2;
+let product = { price: 5, quantity: 2 };
 let total = 0;
 
-let dep = new Set();
-const effect = () => { total = price * quantity };
+// let dep = new Set();
+// const depsMap = new Map();
+const targetMap = new WeakMap();
+const effect = () => { total = product.price * product.quantity };
 
-function track() {
-    dep.add(effect)
+function track(target, key) {
+    let depsMap = targetMap.get(target)
+
+    if (!depsMap) {
+        targetMap.set(target, (depsMap = new Map()))
+        console.log("target map is ", targetMap.get(target));
+    }
+
+    let dep = depsMap.get(key)
+
+    if (!dep) {
+        depsMap.set(key, (dep = new Set()));
+    }
+
+    dep.add(effect);
 }
 
-function trigger() {
-    dep.forEach(effect => effect())
+function trigger(target,key) {
+    const depsMap = targetMap.get(target)
+    if (!depsMap) return
+
+    let dep = depsMap.get(key);
+    console.log("depsMap is:", depsMap);
+
+    if (dep) {
+        dep.forEach(effect => effect())
+    }
 }
+
+track(product, 'price');
+track(product, 'quantity')
+product.quantity = 50;
+trigger(product, 'price');
+console.log("total:", total);
